39a718550548392234860e044483d81812a4ed43
Nate Brennand <natebrennand@gmail.com>
2022-11-14 21:44:42 -0800

    overhaul datasource to match tags

---
+++ csv.to.json.py
@@ -0,0 +1,33 @@
+import os, argparse, csv, json
+
+expected_columns = set([
+    'date',
+    'index',
+    'location',
+    'name',
+    'result',
+])
+
+parser = argparse.ArgumentParser(prog='reformat')
+parser.add_argument('csv_filename')
+args = parser.parse_args()
+
+parsed = []
+
+with open(args.csv_filename, encoding="utf-8") as f:
+    csv_reader = csv.DictReader(f)
+    present_columns = set(map(lambda f: f.lower(), csv_reader.fieldnames))
+
+    if not present_columns.issuperset(expected_columns):
+        print(f"Must have {expected_columns}, found {present_columns}")
+        exit(1)
+
+    for row in csv_reader:
+        row = {
+          k.lower(): v.strip()
+          for k, v in row.items()
+          if k.lower() in expected_columns
+        }
+        parsed.append(row)
+
+print(json.dumps(parsed, sort_keys=True, indent=2, ensure_ascii=False))
